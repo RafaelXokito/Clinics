6b7848e0b5a5f48bec9a34c4e84957cffd649d21
Rafael Mendes Pereira <2191266@my.ipleiria.pt>
2021-10-23 23:01:47 +0100

    Initial commit

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/clinics/HelloApplication.java
@@ -0,0 +1,9 @@
+package pt.ipleiria.estg.dei.ei.dae.clinics;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
