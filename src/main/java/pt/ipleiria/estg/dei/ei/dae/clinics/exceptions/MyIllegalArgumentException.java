b6b7206a92b4243543d1a09c489ecb55e4d0a872
Bruna248 <82589383+Bruna248@users.noreply.github.com>
2021-10-27 18:51:57 +0100

    exceptions folder

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/clinics/exceptions/MyIllegalArgumentException.java
@@ -0,0 +1,7 @@
+package pt.ipleiria.estg.dei.ei.dae.clinics.exceptions;
+
+public class MyIllegalArgumentException extends Exception{
+    public MyIllegalArgumentException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/clinics/exceptions/MyIllegalArgumentExceptionMapper.java
@@ -0,0 +1,17 @@
+package pt.ipleiria.estg.dei.ei.dae.clinics.exceptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.logging.Logger;
+
+@Provider
+public class MyIllegalArgumentExceptionMapper implements ExceptionMapper<MyIllegalArgumentException> {
+    private static final Logger logger = Logger.getLogger("exceptions.MyEntityNotFoundExceptionMapper");
+    @Override
+    public Response toResponse(MyIllegalArgumentException e) {
+        String errorMsg = e.getMessage();
+        logger.warning("ERROR: " + errorMsg);
+        return Response.status(Response.Status.BAD_REQUEST).entity(errorMsg).build();
+    }
+}
